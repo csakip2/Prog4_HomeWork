37a6838dda1ef6b2a2d304d4da33019d072c30f5
Csáki Péter <m13ohg@stud.uni-obuda.hu>
2019-03-27 14:39:01 +0100

    Started implementing generic repository interface

---
+++ C#/Transporter/Transporter.Repository/IRepository.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="IRepository.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Transporter.Repository
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Generic interface for repositories.
+    /// Contains all needed CRUD methods.
+    /// </summary>
+    public class IRepository
+    {
+    }
+}
