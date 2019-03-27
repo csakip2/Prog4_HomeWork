dd05bb02642ce4b5edd5ae97356b2ed83a3685e4
Csáki Péter <m13ohg@stud.uni-obuda.hu>
2019-03-27 13:20:52 +0100

    Started implementing console

--- C#/Transporter/Transporter.Program/Program.cs
+++ C#/Transporter/Transporter.Program/Program.cs
@@ -1,14 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 namespace Transporter.Program
 {
-    class Program
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Main method, handels the menu system.
+        /// </summary>
+        /// <param name="args">Main method argument.</param>
+        public static void Main(string[] args)
         {
         }
     }
