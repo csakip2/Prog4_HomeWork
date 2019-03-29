305cb3178d927ea59348e577634d2f4019029ac7
Csáki Péter <m13ohg@stud.uni-obuda.hu>
2019-03-29 12:47:25 +0100

    Added customer specific repository inteface

---
+++ C#/Transporter/Transporter.Repository/ICustomerRepository.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="ICustomerRepository.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Transporter.Repository
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Repository interface for the customer specific methods.
+    /// </summary>
+    internal interface ICustomerRepository
+    {
+    }
+}
