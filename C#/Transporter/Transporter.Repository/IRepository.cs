759a675c956d2204188158097c460caad1c1a748
Csáki Péter <m13ohg@stud.uni-obuda.hu>
2019-03-29 12:36:10 +0100

    Defined the generic repository interface

--- C#/Transporter/Transporter.Repository/IRepository.cs
+++ C#/Transporter/Transporter.Repository/IRepository.cs
@@ -14,7 +14,32 @@ namespace Transporter.Repository
     /// Generic interface for repositories.
     /// Contains all needed CRUD methods.
     /// </summary>
-    public class IRepository
+    /// <typeparam name="TEntity">Generic TEntity for all enttities.</typeparam>
+    public interface IRepository<TEntity>
     {
+        /// <summary>
+        /// Returns all elements of a table as IQueryable.
+        /// </summary>
+        /// <returns>All element as IQueryable.</returns>
+        IQueryable<TEntity> GetTable();
+
+        /// <summary>
+        /// Inserts an Entity into the table.
+        /// </summary>
+        /// <param name="entity">The entity to insert.</param>
+        void Insert(TEntity entity);
+
+        /// <summary>
+        /// Removes the given entity.
+        /// </summary>
+        /// <param name="entity">The entity to remove.</param>
+        void Remove(TEntity entity);
+
+        /// <summary>
+        /// Removes the 1st given entity and then inserts the 2nd given entity.
+        /// </summary>
+        /// <param name="fromEntity">Removes this one.</param>
+        /// <param name="toEntity">Inserts this one.</param>
+        void Change(TEntity fromEntity, TEntity toEntity);
     }
 }
