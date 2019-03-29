76d2e720379922b29ab32193209dd15823711e01
Csáki Péter <m13ohg@stud.uni-obuda.hu>
2019-03-29 13:02:02 +0100

    Defined ICustomerRepository

--- C#/Transporter/Transporter.Repository/ICustomerRepository.cs
+++ C#/Transporter/Transporter.Repository/ICustomerRepository.cs
@@ -13,7 +13,27 @@ namespace Transporter.Repository
     /// <summary>
     /// Repository interface for the customer specific methods.
     /// </summary>
-    internal interface ICustomerRepository
+    internal interface ICustomerRepository : IRepository<Data.CUSTOMER>
     {
+        /// <summary>
+        /// Changes a customers adress.
+        /// </summary>
+        /// <param name="id">The ID of the customer.</param>
+        /// <param name="newAdress">The customers new adress.</param>
+        void ChangeAdress(int id, string newAdress);
+
+        /// <summary>
+        /// Changes the customers phone number.
+        /// </summary>
+        /// <param name="id">The ID of the customer.</param>
+        /// <param name="newPhoneNum">The new phone number of the customer.</param>
+        void ChangePhoneNum(int id, string newPhoneNum);
+
+        /// <summary>
+        /// Changes the customers e-mail adress.
+        /// </summary>
+        /// <param name="id">The ID of the customer.</param>
+        /// <param name="newEmail">The new e-mail adress.</param>
+        void ChangeEmail(int id, string newEmail);
     }
 }
