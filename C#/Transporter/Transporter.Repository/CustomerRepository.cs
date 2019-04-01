3faac8e52fa05e17bd38685afb6c14952519fe62
Csáki Péter <m13ohg@stud.uni-obuda.hu>
2019-04-01 15:38:01 +0200

    Added interface to repository

--- C#/Transporter/Transporter.Repository/CustomerRepository.cs
+++ C#/Transporter/Transporter.Repository/CustomerRepository.cs
@@ -9,8 +9,41 @@ namespace Transporter.Repository
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+    using Transporter.Data;
 
-    internal class CustomerRepository
+    /// <summary>
+    /// Repository for Customer table.
+    /// </summary>
+    internal class CustomerRepository : ICustomerRepository
     {
+        public void ChangeAdress(int id, string newAdress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeEmail(int id, string newEmail)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangePhoneNum(int id, string newPhoneNum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<CUSTOMER> GetTable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(CUSTOMER entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(CUSTOMER entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
