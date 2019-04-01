0bf801a47af270a03db1d6d416f3feb4b97d6ef9
Csáki Péter <m13ohg@stud.uni-obuda.hu>
2019-04-01 15:03:05 +0200

    fixed StyleCop warnings

--- C#/Transporter/Transporter.Repository/IPakageRepository.cs
+++ C#/Transporter/Transporter.Repository/IPakageRepository.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// <copyright file="IPakageRepository.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace Transporter.Repository
 {
-    interface IPakageRepository
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Repository interface for pakage specific methods.
+    /// </summary>
+    internal interface IPakageRepository : IRepository<Data.PAKAGE>
     {
     }
 }
