d51fa7b0d62b4f7a9df8fe7378f684d42e501135
Csáki Péter <m13ohg@stud.uni-obuda.hu>
2019-04-01 15:14:55 +0200

    Finished IPakageRepository

--- C#/Transporter/Transporter.Repository/IPakageRepository.cs
+++ C#/Transporter/Transporter.Repository/IPakageRepository.cs
@@ -15,5 +15,25 @@ namespace Transporter.Repository
     /// </summary>
     internal interface IPakageRepository : IRepository<Data.PAKAGE>
     {
+        /// <summary>
+        /// Changes the pakages driver.
+        /// </summary>
+        /// <param name="id">Pakages id.</param>
+        /// <param name="newDriverId">ID of the new driver.</param>
+        void ChangeDriver(int id, int newDriverId);
+
+        /// <summary>
+        /// Returns the driver of the pakage.
+        /// </summary>
+        /// <param name="id">The id of the pakage.</param>
+        /// <returns>Returns the driver as a Data.DRIVER object.</returns>
+        Data.DRIVER GetDriver(int id);
+
+        /// <summary>
+        /// Returns the route of the pakage.
+        /// </summary>
+        /// <param name="id">The id of the pakage.</param>
+        /// <returns>The route in "[0]from, [1](through driver), [2]to" string array.</returns>
+        string[] GetRoute(int id);
     }
 }
