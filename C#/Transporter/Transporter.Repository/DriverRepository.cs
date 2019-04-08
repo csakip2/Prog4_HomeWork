4ca062a5a1aaa9ca5e8a24a0d80c3728caaf2fe8
Csáki Péter <m13ohg@stud.uni-obuda.hu>
2019-04-08 14:53:57 +0200

    Created DriverRepository

---
+++ C#/Transporter/Transporter.Repository/DriverRepository.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="DriverRepository.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Transporter.Repository
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class DriverRepository
+    {
+    }
+}
