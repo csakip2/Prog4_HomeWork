5c0d53988c028583615ddf2861874664208a1ad3
Csáki Péter <m13ohg@stud.uni-obuda.hu>
2019-04-08 15:44:02 +0200

    Added more option to ILogic.

--- C#/Transporter/Transporter.Logic/Ilogic.cs
+++ C#/Transporter/Transporter.Logic/Ilogic.cs
@@ -32,5 +32,35 @@ namespace Transporter.Logic
         /// </summary>
         /// <returns>All elements in a tring array.</returns>
         string[] RetriveDrivers();
+
+        /// <summary>
+        /// Adds a new customer to the table.
+        /// </summary>
+        /// <param name="name">The new customers name.</param>
+        /// <param name="adress">The new customers adress.</param>
+        /// <param name="phoneNum">The new customers phone number.</param>
+        /// <param name="e_mail">The new customers e-mail adress.</param>
+        void AddCustomer(string name, string adress, string phoneNum, string e_mail);
+
+        /// <summary>
+        /// Adds a new pakage to the table.
+        /// </summary>
+        /// <param name="senderName">The new pakages senders name.</param>
+        /// <param name="senderAdress">The new pakages senders adress.</param>
+        /// <param name="receiverName">The new pakages recivers name.</param>
+        /// <param name="receiverAdress">The new pakages recivers adress.</param>
+        /// <param name="weight">The new pakages weight.</param>
+        /// <param name="size">The new pakages size.</param>
+        void AddPakage(string senderName, string senderAdress, string receiverName, string receiverAdress, int weight, string size);
+
+        /// <summary>
+        /// Adds a new driver to the table.
+        /// </summary>
+        /// <param name="name">The new drivers name.</param>
+        /// <param name="adress">The new drivers adress.</param>
+        /// <param name="birthDate">The new drivers birth date.</param>
+        /// <param name="licencePlate">The new drivers licence plate number.</param>
+        /// <param name="phoneNum">The new drivers phone number.</param>
+        void AddDriver(string name, string adress, DateTime birthDate, string licencePlate, string phoneNum);
     }
 }
