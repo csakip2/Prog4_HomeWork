2f41a4950bad4c4c556c7bcacda19068ee6df72c
Csáki Péter <m13ohg@stud.uni-obuda.hu>
2019-04-08 14:55:20 +0200

    Added interface to repository

--- C#/Transporter/Transporter.Repository/DriverRepository.cs
+++ C#/Transporter/Transporter.Repository/DriverRepository.cs
@@ -9,8 +9,41 @@ namespace Transporter.Repository
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+    using Transporter.Data;
 
-    class DriverRepository
+    /// <summary>
+    /// Repository for the driver table.
+    /// </summary>
+    class DriverRepository : IDriverRepository
     {
+        public void ChangeAdress(int id, string newAdress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeLicPlate(int id, string newLicPlate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangePhoneNum(int id, string newPhoneNum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<DRIVER> GetTable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(DRIVER entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(DRIVER entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
