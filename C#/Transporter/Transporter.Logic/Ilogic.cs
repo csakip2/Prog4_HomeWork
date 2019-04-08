d79ded79de69cba6dd4e8894558655f3b03c87ce
Csáki Péter <m13ohg@stud.uni-obuda.hu>
2019-04-08 15:23:49 +0200

    Created ILogic interface

---
+++ C#/Transporter/Transporter.Logic/Ilogic.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="Ilogic.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Transporter.Logic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Logic interface.
+    /// </summary>
+    internal interface ILogic
+    {
+    }
+}
