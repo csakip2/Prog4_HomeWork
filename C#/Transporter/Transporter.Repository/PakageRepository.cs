574d3031ae9672dc6720c61d4a7aae3bbd77bf4a
Csáki Péter <m13ohg@stud.uni-obuda.hu>
2019-04-08 13:56:43 +0200

    Created PakageRepository

---
+++ C#/Transporter/Transporter.Repository/PakageRepository.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="PakageRepository.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Transporter.Repository
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Repository for Pakages table.
+    /// </summary>
+    internal class PakageRepository
+    {
+    }
+}
