eb1fdc28df20c49bbcd025ee793906f97131c738
Csáki Péter <m13ohg@stud.uni-obuda.hu>
2019-04-10 20:52:33 +0200

    Added ILogic interface to Logic class

--- C#/Transporter/Transporter.Logic/Logic.cs
+++ C#/Transporter/Transporter.Logic/Logic.cs
@@ -13,7 +13,96 @@ namespace Transporter.Logic
     /// <summary>
     /// Logic class. Handels repository methods.
     /// </summary>
-    class Logic
+    internal class Logic : ILogic
     {
+        public void AddCustomer(string name, string adress, string phoneNum, string e_mail)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddDriver(string name, string adress, DateTime birthDate, string licencePlate, string phoneNum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddPakage(string senderName, string senderAdress, string receiverName, string receiverAdress, int weight, string size)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeCustomerAdress(string name, string oldAdress, string newAdress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeCustomerEmail(string name, string adress, string newEmail)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeCustomerPhoneNum(string name, string adress, string newPhoneNum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeDriverAdress(string name, string oldAdress, string newAdress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeDriverLicPlate(string name, string adress, string newLicPlate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeDriverPhoneNum(string name, string adress, string newPhoneNum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangePakageDriver(int id, int newDriver_id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] GetPakageDriver(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] GetPakageRoute(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveCustomer(string name, string adress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveDriver(string name, string adress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemovePakage(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] RetriveCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] RetriveDrivers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] RetrivePakages()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
