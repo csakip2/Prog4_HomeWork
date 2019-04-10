0ff2ca87bc409fef53f5b85d1e07802618a944af
Csáki Péter <m13ohg@stud.uni-obuda.hu>
2019-04-10 20:51:33 +0200

    Added Logic class

---
+++ C#/Transporter/Transporter.Logic/Logic.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="Logic.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Transporter.Logic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Logic class. Handels repository methods.
+    /// </summary>
+    class Logic
+    {
+    }
+}
