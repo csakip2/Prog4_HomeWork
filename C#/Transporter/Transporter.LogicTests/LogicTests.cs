7a5efaa450b51bc416faa63604c10607a29214a5
Csáki Péter <m13ohg@stud.uni-obuda.hu>
2019-04-24 16:13:30 +0200

    Started on Logic tests

---
+++ C#/Transporter/Transporter.LogicTests/LogicTests.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="LogicTests.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Transporter.LogicTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Test class for Logic.
+    /// </summary>
+    public class LogicTests
+    {
+    }
+}
