c339417d181fd4951a4ad9d8adf0b22f2e3613e2
Csáki Péter <m13ohg@stud.uni-obuda.hu>
2019-05-09 20:50:37 +0200

    Implemented Pakage modell

--- JAVA/Transporter.JavaWeb/src/java/Modell/Pakage.java
+++ JAVA/Transporter.JavaWeb/src/java/Modell/Pakage.java
@@ -1,5 +1,27 @@
 package Modell;
 
 public class Pakage {
+    private int weight;
+    private String size;
+
+    public Pakage() {
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+    
     
 }
