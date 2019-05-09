0d72801eca00ab068feabfe5ac8350f3296fdb0b
Csáki Péter <m13ohg@stud.uni-obuda.hu>
2019-05-09 21:23:20 +0200

    Added company modell

---
+++ JAVA/Transporter.JavaWeb/src/java/Modell/Company.java
@@ -0,0 +1,30 @@
+package Modell;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "company")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Company {
+    private static int next_ID;
+    @XmlElement
+    private int id;
+    private int basePrice;
+
+    public Company(int basePrice) {
+        this.id = next_ID++;
+        this.basePrice = basePrice;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getBasePrice() {
+        return basePrice;
+    }
+    
+    
+}
