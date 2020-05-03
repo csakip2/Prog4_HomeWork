1bcc27445f235a0fb6a6e158cbd17af0825464ce
Csáki Péter <m13ohg@stud.uni-obuda.hu>
2020-05-03 16:36:32 +0200

    Added Customer Form model

---
+++ C#/Transporter/Transporter.Web/Models/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Transporter.Web.Models
+{
+    // Form model
+    public class Customer
+    {
+        [Display(Name = "Customer ID")]
+        [Required]
+        public int Id { get; set; }
+
+        [Display(Name = "Customer Name")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Customer Adress")]
+        [Required]
+        public string Adress { get; set; }
+
+        [Display(Name = "Customer Phone Number")]
+        [Required]
+        [StringLength(12, MinimumLength = 11)]
+        public string PhoneNum { get; set; }
+
+        [Display(Name = "Customer E-Mail Adress")]
+        [Required]
+        public string EMail { get; set; }
+    }
+}
