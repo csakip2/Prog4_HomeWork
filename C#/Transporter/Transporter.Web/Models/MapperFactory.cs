075bcdd6b8d8038662392bef8367a3d2b44baa21
Csáki Péter <m13ohg@stud.uni-obuda.hu>
2020-05-03 17:05:26 +0200

    Added MapperFactory

---
+++ C#/Transporter/Transporter.Web/Models/MapperFactory.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Transporter.Web.Models
+{
+    public class MapperFactory
+    {
+        public static IMapper CreateMapper()
+        {
+            var config = new MapperConfiguration(cfg => 
+            {
+                cfg.CreateMap<Transporter.Data.CUSTOMER, Transporter.Web.Models.Customer>().
+                ForMember(dest => dest.Id, map => map.MapFrom(src => src.CUSTOMER_ID)).
+                ForMember(dest => dest.Name, map => map.MapFrom(src => src.CNAME)).
+                ForMember(dest => dest.Adress, map => map.MapFrom(src => src.CADRESS)).
+                ForMember(dest => dest.PhoneNum, map => map.MapFrom(src => src.CPHONE_NUM == null ? "" : src.CPHONE_NUM)).
+                ForMember(dest => dest.EMail, map => map.MapFrom(src => src.CE_MAIL));
+            });
+
+            return config.CreateMapper();
+        }
+    }
+}
