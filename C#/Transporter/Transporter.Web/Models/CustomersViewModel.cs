85137b91208bfb07bec9e74b1d6f7d726aa475ae
Csáki Péter <m13ohg@stud.uni-obuda.hu>
2020-05-03 16:45:08 +0200

    Added CustomersViewModel

---
+++ C#/Transporter/Transporter.Web/Models/CustomersViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Transporter.Web.Models
+{
+    public class CustomersViewModel
+    {
+        public Customer EditedCustomer { get; set; }
+        public List<Customer> ListOfCustomers { get; set; }
+    }
+}
