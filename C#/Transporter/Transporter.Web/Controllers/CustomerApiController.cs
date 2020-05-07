535ac758556dcc50d27b52309f88b93c804cedc5
Csáki Péter <m13ohg@stud.uni-obuda.hu>
2020-05-07 11:56:55 +0200

    Added CRUD to CustomerApiController

--- C#/Transporter/Transporter.Web/Controllers/CustomerApiController.cs
+++ C#/Transporter/Transporter.Web/Controllers/CustomerApiController.cs
@@ -12,6 +12,11 @@ namespace Transporter.Web.Controllers
 {
     public class CustomerApiController : ApiController
     {
+        public class ApiResult
+        {
+            public bool OperationResult { get; set; }
+        }
+
         ILogic logic;
         IMapper mapper;
         public CustomerApiController()
@@ -20,11 +25,41 @@ namespace Transporter.Web.Controllers
             mapper = MapperFactory.CreateMapper();
         }
 
-        // GET: api/CustomerApi
-        public IEnumerable<Models.Customer> Get()
+        // GET: api/CustomerApi/all
+        [ActionName("all")]
+        [HttpGet]
+        public IEnumerable<Models.Customer> GetAll()
         {
             var customers = logic.GetCustomerList();
             return mapper.Map<IList<Data.CUSTOMER>, List<Models.Customer>>(customers);
         }
+
+        // GET: api/CustomerApi/del/5
+        [ActionName("del")]
+        [HttpGet]
+        public ApiResult DelOneCustomer(int id)
+        {
+            bool res = logic.RemoveCustomer(id);
+            return new ApiResult() { OperationResult = res };
+        }
+
+        // GET: api/CustomerApi/add + customer
+        [ActionName("add")]
+        [HttpPost]
+        public ApiResult AddOneCustomer(Customer customer)
+        {
+            logic.AddCustomer(customer.Name, customer.Adress, customer.PhoneNum, customer.EMail);
+            return new ApiResult() { OperationResult = true };
+        }
+
+        // GET: api/CustomerApi/mod + customer
+        [ActionName("mod")]
+        [HttpPost]
+        public ApiResult ModOneCustomer(Customer customer)
+        {
+            logic.ChangeCustomer(customer.Id, customer.Name, customer.Adress, customer.PhoneNum, customer.EMail);
+            return new ApiResult() { OperationResult = true };
+        }
+
     }
 }
